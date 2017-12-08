8a3d6f8c3c250f5d20e35213da2de33acd2df188
DOUGLAS C DA SILVA <douglascdasilva@MacBook-Air-de-DOUGLAS.local>
2017-12-08 20:02:27 -0200

    the architecture of the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VehicleListComponent } from './scenes/vehicles/vehicle-list/vehicle-list.component';
+import { VehicleRegisterComponent } from './scenes/vehicles/vehicle-register/vehicle-register.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		VehicleListComponent,
+		VehicleRegisterComponent
+	],
+	imports: [
+		BrowserModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/scenes/vehicles/models/vehicle.model.ts
@@ -0,0 +1,8 @@
+export class Vehicle {
+	combustivel: string;
+	imagem: string;
+	marca: string;
+	modelo: string;
+	placa: string;
+	valor: number;
+}
